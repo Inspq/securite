f24afcbdc1e84aea43c2328bb6a642ab86f604d2
gauphi01@inspq.qc.ca <gauphi01@inspq.qc.ca@inspq-6111.inspq.qc.ca>
2017-03-25 08:03:47 -0400

    Sx5-25 Creation d'image Docker pour les agents afin de faciliter la portabilité e la POC.

---
+++ deploiement/Sx5Deploiement/Docker/OpenAMDotnetAgent/configAgent.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Script d'installation de l'agent web d'OpenAM
+# Arguments: 
+#	virtualhost name: Nom a donner a la configuration de l'hôte virtuel
+#	dll: Fichier dll dotnet core a exécuter
+
+set -e
+VIRTUAL_HOST_NAME=$1
+DOTNET_DLL=$2
+AGENT_PROTO=`echo $AGENT_URL | awk -F: '{print $1}'`
+AGENT_HOSTNAME=`echo $AGENT_URL | awk -F '[/:]' '{print $4}'`
+AGENT_PORT=`echo $AGENT_URL | awk -F '[/:]' '{print $5}'`
+AGENT_URI=`echo $AGENT_URL | awk -F/ '{print $4}'`
+
+if [ ! -e /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf ]; then
+    echo "<VirtualHost *:$AGENT_PORT>" > /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "    ServerName $AGENT_HOSTNAME" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "    ProxyRequests Off" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf 
+    echo "    <Proxy *>" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "            Order deny,allow" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "            Allow from all" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "    </Proxy>" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "    ProxyPass / http://localhost:50633/" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "    ProxyPassReverse / http://localhost:50633/" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+    echo "</VirtualHost>" >> /etc/httpd/conf.d/${VIRTUAL_HOST_NAME}.conf
+
+    sed -i s'/Listen 80/Listen '"${AGENT_PORT}"'/g' /etc/httpd/conf/httpd.conf
+fi
+
+backupfiles=(/etc/httpd/conf/httpd.conf_amagent*)
+if [ ! -e "${backupfiles[0]}" ]; then
+	echo $AGENT_PWD > /tmp/passwd.txt
+	chmod 400 /tmp/passwd.txt
+
+    # Attendre que le serveur OpenAM soit disponible
+    sleep 30
+	curl -f -m 60 --retry 5 --retry-delay 30 $OPENAM_URL
+
+    /opt/web_agents/apache24_agent/bin/agentadmin --s "/etc/httpd/conf/httpd.conf" \
+                "$OPENAM_URL" "$AGENT_URL" "/" "$AGENT_PROFILE_NAME" \
+                "/tmp/passwd.txt" --acceptLicence --changeOwner
+fi
+apachectl
+dotnet exec ${DOTNET_DLL}
