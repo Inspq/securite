9513bd3b68196ad62e76a653dfd85a38b412718c
Philippe Gauthier <gauphi01@inspq.qc.ca@inspq-6111.inspq.qc.ca>
2017-06-22 14:57:57 -0400

    Ajustement pour Keycloak

--- pocdotnetcore/services/Startup.cs
+++ pocdotnetcore/services/Startup.cs
@@ -32,9 +32,9 @@ namespace services
         {
             loggerFactory.AddConsole();
             string authority = "http://login.bicycle2.inspq.qc.ca:18080/openam/oauth2/";
-            if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("OPENAM_URL")))
+            if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("OIDC_AUTHORITY_URL")))
             {
-                authority = Environment.GetEnvironmentVariable("OPENAM_URL") + "/oauth2/";
+                authority = Environment.GetEnvironmentVariable("OIDC_AUTHORITY_URL");
             }
 
             string clientId = Environment.GetEnvironmentVariable("OIDC_CLIENT_ID");
